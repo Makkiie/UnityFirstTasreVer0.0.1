08b023d9068672b9d4118c1051bc2d77a8e3582e
Rushi57 <rushihanzo07@gmail.com>
2025-05-21 12:54:01 +0800

    sampleSceneforLVL1
    Sample palang

---
+++ Assets/Codes/SlotsCode/Slots.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slots : MonoBehaviour
+{
+    public Image iconDisplay;
+    //private IngredientsSO currentIngrediets;
+    private GameObject currentPrefab;
+
+}
